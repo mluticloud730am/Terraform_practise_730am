024aef66940f77672050da440377b66e488a1604
Rakesh Arveti <multicloud730am@gmail.com>
2026-03-13 08:38:20 +0530

    bucketstatecreateand bucketcreate

---
+++ Terraform-Day6-Common-state/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-rakesh"
+    key            = "day6commonstate/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
