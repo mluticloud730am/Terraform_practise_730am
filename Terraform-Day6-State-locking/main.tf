f5fdcb68a39f6a08c343e1da248f08efd0477c9e
Rakesh Arveti <multicloud730am@gmail.com>
2026-03-13 08:16:07 +0530

    updated

--- Terraform-Day6-State-locking/main.tf
+++ Terraform-Day6-State-locking/main.tf
@@ -5,3 +5,14 @@ resource "aws_vpc" "name" {
     Name = "Rakesh_Dev_VPC"
   }
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
